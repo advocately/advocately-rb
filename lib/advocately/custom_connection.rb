8fa8e26833bb81a45294a03caead35c606da8bc7
Lachlan Priest <lachlanpriest@gmail.com>
2019-07-22 19:45:07 +0100

    Bugfix custom connection

--- lib/advocately/custom_connection.rb
+++ lib/advocately/custom_connection.rb
@@ -1,9 +1,9 @@
 class CustomConnection < JsonApiClient::Connection
-  def run(request_method, path, params = {}, headers = {})
+  def run(request_method, path, params: nil, headers: {}, body: nil)
     token = Advocately::Base.api_token
     account_id = Advocately::Base.app_id
     headers["Authorization"] = "Token token=#{token}, account_id=#{account_id}"
 
-    super(request_method, path, params, headers)
+    super(request_method, path, params: params, headers: headers, body: body)
   end
 end
